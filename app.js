daac7075b703a71eff6355a0e38bba1ad1af3f94
Kateryna02 <150830031+Kateryna02@users.noreply.github.com>
2024-06-09 22:41:25 +0200

    new

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import mongoose from 'mongoose';
 
 import contactsRouter from "./routes/contactsRouter.js";
+import { log } from "console";
 
 const app = express();
 
@@ -21,6 +23,12 @@ app.use((err, req, res, next) => {
   res.status(status).json({ message });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
+const DB_HOST = "mongodb+srv://anna:Gg65lMAPzYYjEpz7@cluster0.cfnv3tz.mongodb.net/db-contacts?retryWrites=true&w=majority&appName=Cluster0"
+mongoose.connect(DB_HOST)
+.then(() => {
+  app.listen(3000, () => {
+    console.log("Server is running. Use our API on port: 3000");
+  });
+}).catch((error) => {
+  console.log(error.message);
 });
