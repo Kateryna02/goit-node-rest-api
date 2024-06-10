cd758589ed3c4ac147a73bdd9d368c5c2f01e23a
Kateryna02 <150830031+Kateryna02@users.noreply.github.com>
2024-06-10 15:58:58 +0200

    add favorite

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -4,7 +4,7 @@ import express from "express";
 import contactsControllers from "../controllers/contactsControllers.js";
 import isEmptyBody from "../midalwars/isEmptyBody.js";
 import validateBody from "../helpers/validateBody.js";
-import { createContactSchema, updateContactSchema } from "../schemas/contactsSchemas.js";
+import { createContactSchema, updateContactSchema , updateFavoriteSchema} from "../schemas/contactsSchemas.js";
 
 const contactsRouter = express.Router();
 
@@ -13,5 +13,7 @@ contactsRouter.get("/:id", contactsControllers.getOneContact);
 contactsRouter.post("/", isEmptyBody, validateBody(createContactSchema), contactsControllers.createContact);
 contactsRouter.put("/:id", isEmptyBody, validateBody(updateContactSchema), contactsControllers.updateContact);
 contactsRouter.delete("/:id", contactsControllers.deleteContact);
+contactsRouter.patch("/:contactId/favorite", validateBody(updateFavoriteSchema), contactsControllers.updateStatusContact);
+
 
 export default contactsRouter;
